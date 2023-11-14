7bc833aa6fb95f6520a161a1a6b3fbf885af6fc1
Tony <tony.w.trinh3@gmail.com>
2023-11-14 16:48:16 -0500

    fixed spacing above img

--- components/ui/airwayscomponents/airlineInfoCard.tsx
+++ components/ui/airwayscomponents/airlineInfoCard.tsx
@@ -11,11 +11,11 @@ const AirlineInfoCard = ({
   imgSrc: any;
 }) => {
   return (
-    <Card className="flex sm:w-[340px] h-auto border-0 relative flex-col justify-center items-center animate-fade-in grid-rows-2 bg-slate-900 z-0 !rounded-none">
+    <Card className="flex sm:w-[340px] sm:h-[430px] border-0 flex-col  animate-fade-in grid-rows-2 bg-slate-900 z-0 !rounded-none">
       <CardHeader className="!space-y-0 !p-0">
-        <img src={imgSrc.src} className="mx-auto" />
+        <img src={imgSrc.src} className="w-full h-full" />
       </CardHeader>
-      <div className="py-[3rem] px-[1rem]">
+      <div className="p-[3rem]">
         <CardTitle className="flex justify-center">
           <p className="font-bold text-3xl text-gray-300 text-center">{headerTitleText}</p>
         </CardTitle>
