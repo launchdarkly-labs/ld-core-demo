f9ebb969a9c7fcb450881edcff8c373a1e7c2361
Brian Rinaldi <brian.rinaldi@gmail.com>
2023-11-14 16:10:27 -0500

    home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,22 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import {  AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { ArrowRight, Globe } from "lucide-react";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import { CSNav } from "@/components/ui/csnav";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
 
   const goToBank = () => {
-    router.push('/bank')
-  }
+    router.push("/bank");
+  };
 
   const goToNext = () => {
-    router.push('/devs')
-  }
+    router.push("/devs");
+  };
 
   const variants = {
     hidden: { opacity: 0 },
@@ -28,7 +27,7 @@ export default function Home() {
       },
     },
   };
-  
+
   const childVariants = {
     hidden: { opacity: 0 },
     show: { opacity: 1 },
@@ -47,55 +46,42 @@ export default function Home() {
   };
 
   return (
-    <AnimatePresence mode="wait">
-    <motion.main
-        className={`relative flex h-screen flex-col items-center justify-center bg-ldblack`}
-        initial="initial"
-        animate="in"
-        exit="out"
-        variants={pageVariants}
-        transition={pageTransition}
-      >
-        <div className="absolute top-0 w-full text-white flex h-20 shadow-2xl ">
-            <div className="ml-4 flex items-center text-3xl">
-            <CSNav />
-            
-           <img src="ld-white-wide.png" className="h-1/2 pl-4" /> 
-           </div>
+    <main className="h-full flex-col items-center justify-center bg-ldblack">
+      <div className="w-full text-white flex h-20 shadow-2xl">
+        <div className="ml-4 flex items-center text-3xl">
+          <CSNav />
+
+          <img src="ld-white-wide.png" className="h-1/2 pl-4" />
         </div>
-      
-      <div onClick={goToNext} className="border-2 border-white/20 hover:border-white/60 absolute p-4 right-2 top-1/2 transform -translate-y-1/2">
-      <ArrowRight className="text-zinc-500" size={36} />
       </div>
-      <div className="mx-32">
-      <motion.div
-        initial={{ scaleX: 0 }} // start with no width
-        animate={{ scaleX: 1 }} // animate to full width
-        transition={{ duration: 1 }} // duration of the animation
-        style={{ overflow: "hidden", transformOrigin: "left" }} // hide the overflowing text and set the origin of transformation to left
-      >
-        <p className="text-6xl font-sohnemono herotext">
-          How fast could you innovate if you didn't have to worry about risk?
-        </p>
-      </motion.div>
-      <motion.div
-        className="flex flex-row gap-4 items-center justify-center pt-10"
-        variants={variants}
-        initial="hidden"
-        animate="show"
-      >
-        {["Card-1-New.png", "Card-2-New.png", "Card-3-New.png", "Card-4-New.png"].map((card, index) => (
-          <motion.img
-            key={card}
-            src={card}
-            alt={`Card ${index + 1}`}
-            className="w-1/4"
-            variants={childVariants}
-          />
-        ))}
-      </motion.div>
+
+      <div className="banner">
+        <div>
+          <h1>Connecting code to customers</h1>
+          <p>
+            Maximize the value of every software feature through safer releases,
+            targeted experiences, experimentation, and more.
+          </p>
+        </div>
       </div>
-    </motion.main>
-    </AnimatePresence>
+      <div className="flex flex-wrap items-center justify-center pt-10 mx-auto w-1/2 gap-8">
+        <div className="flex flex-row gap-8">
+          <div className="w-1/2">
+            <img src="Card-1-New.png" alt="" />
+          </div>
+          <div className="w-1/2">
+            <img src="Card-2-New.png" alt="" />
+          </div>
+        </div>
+        <div className="flex flex-row gap-8">
+          <div className="w-1/2">
+            <img src="Card-3-New.png" alt="" />
+          </div>
+          <div className="w-1/2">
+            <img src="Card-4-New.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
