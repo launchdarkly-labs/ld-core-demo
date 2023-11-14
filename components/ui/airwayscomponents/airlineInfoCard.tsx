7da17c98ef4eaece71a2c489a2b5014210731a8e
Tony <tony.w.trinh3@gmail.com>
2023-11-14 17:22:46 -0500

    tempt solution for bottom anchor arrow

--- components/ui/airwayscomponents/airlineInfoCard.tsx
+++ components/ui/airwayscomponents/airlineInfoCard.tsx
@@ -12,9 +12,9 @@ const AirlineInfoCard = ({
   imgSrc: any;
 }) => {
   return (
-    <Card className="flex sm:w-[340px] sm:h-[430px] border-0 flex-col relative animate-fade-in grid-rows-2 bg-slate-900 z-0 !rounded-none">
+    <Card className="flex sm:w-[340px] sm:h-[460px] border-0 flex-col animate-fade-in grid-rows-2 bg-slate-900 z-0 !rounded-none relative">
       <CardHeader className="!space-y-0 !p-0">
-        <img src={imgSrc.src} className="w-full h-full" />
+        <img src={imgSrc.src} className="w-full h-full object-fit" />
       </CardHeader>
       <div className="p-[2.5rem] flex flex-col gap-y-2">
         <CardTitle className="flex justify-center !font-normal">
@@ -24,7 +24,7 @@ const AirlineInfoCard = ({
           <p className="text-gray-300 pt-2 text-lg !font-normal text-center">{subtitleText}</p>
         </CardContent>
 
-        <ArrowRight className="h-6 w-6 text-airlinePurple cursor-pointer mt-auto" />
+        <ArrowRight className="h-6 w-6 text-airlinePurple cursor-pointer mt-auto absolute bottom-0 mb-6" />
       </div>
     </Card>
   );
