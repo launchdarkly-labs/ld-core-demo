21ace80a76511c205614aa27927c98b9867565bf
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2023-11-15 14:46:18 -0500

    Revert "Merge pull request #5 from launchdarkly-labs/tony_airline_responsiveness"
    This reverts commit ebb15c43a1c617929f387d7a55a93dca6afb3d9b, reversing
    changes made to 11b552829ef9544d3dc27f507f704a8eec1ad52d.

--- pages/airways.tsx
+++ pages/airways.tsx
@@ -1,5 +1,10 @@
 import { Inter } from "next/font/google";
-import { ArrowRightCircle, CalendarIcon, MoveHorizontalIcon, Plane } from "lucide-react";
+import {
+  ArrowRightCircle,
+  CalendarIcon,
+  MoveHorizontalIcon,
+  Plane,
+} from "lucide-react";
 import { useContext, useEffect, useRef, useState } from "react";
 import AirportPicker from "@/components/ui/airwayscomponents/airportPicker";
 import { motion, useAnimation, useInView } from "framer-motion";
@@ -8,7 +13,11 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useToast } from "@/components/ui/use-toast";
 import { Toaster } from "@/components/ui/toaster";
 import { Calendar } from "@/components/ui/calendar";
-import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
 import { useFlags } from "launchdarkly-react-client-sdk";
 import { CSNav } from "@/components/ui/csnav";
 import { RegistrationForm } from "@/components/ui/airwayscomponents/stepregistration";
@@ -20,7 +29,6 @@ import hotAirBalloonImg from "@/assets/img/airways/hotairBalloon.jpg";
 import airplaneDining from "@/assets/img/airways/airplaneDining.jpg";
 import { FlightCalendar } from "@/components/ui/airwayscomponents/flightCalendar";
 import { Button } from "@/components/ui/button";
-import AirlineHero from "@/components/ui/airwayscomponents/airlineHero";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -41,36 +49,28 @@ export default function Home() {
     setShowSearch(true);
   }
 
+
   useEffect(() => {
-    console.log(date);
-    console.log(bookedTrips);
+console.log(date)
+console.log(bookedTrips)
   }, [bookedTrips]);
 
+
   function bookTrip() {
-    const startDate = `${
-      date.from.getMonth() + 1
-    }/${date.from.getDate()}/${date.from.getFullYear()}`;
+    const startDate = `${date.from.getMonth() + 1}/${date.from.getDate()}/${date.from.getFullYear()}`;
     const returnDate = `${date.to.getMonth() + 1}/${date.to.getDate()}/${date.to.getFullYear()}`;
     const tripIdOutbound = Math.floor(Math.random() * 900) + 100; // Generate a random 3 digit number for outbound trip
     const tripIdReturn = Math.floor(Math.random() * 900) + 100; // Generate a random 3 digit number for return trip
-
-    const outboundTrip = {
-      id: tripIdOutbound,
-      from: fromLocation,
-      to: toLocation,
-      depart: startDate,
-      type: "Outbound",
-    };
-    const returnTrip = {
-      id: tripIdReturn,
-      from: toLocation,
-      to: fromLocation,
-      depart: returnDate,
-      type: "Return",
-    };
-
-    setBookedTrips([...bookedTrips, outboundTrip, returnTrip]);
-
+  
+    const outboundTrip = { id: tripIdOutbound, from: fromLocation, to: toLocation, depart: startDate, type: 'Outbound' };
+    const returnTrip = { id: tripIdReturn, from: toLocation, to: fromLocation, depart: returnDate, type: 'Return' };
+  
+    setBookedTrips([
+      ...bookedTrips,
+      outboundTrip,
+      returnTrip
+    ]);
+  
     toast({
       title: "Flight booked",
       description: `Your round trip from ${fromLocation} to ${toLocation} and back has been booked.`,
@@ -97,65 +97,68 @@ export default function Home() {
         exit="out"
         variants={pageVariants}
         transition={pageTransition}
-        className={`flex h-screen  text-white flex-col font-audimat`}
+        className={`flex h-screen bg-gradient-airways text-white flex-col font-audimat`}
       >
         <NavBar variant={"airlines"} />
-        <div className="flex flex-row py-20  space-x-20 items-center bg-gradient-airways">
-          <div className="flex items-center mx-auto">
-            <motion.div
-              initial={{ scale: 0.25, opacity: 0 }}
-              animate={{ scale: 1, opacity: 1 }}
-              transition={{ duration: 0.25 }}
-              className="flex"
-            >
-              <div className="relative">
-                <button
-                  onClick={() => {
-                    setActiveField("from");
-                    setShowSearch(true);
-                  }}
-                >
-                  <p className="text-3xl md:text-4xl xl:text-6xl font-audimat px-4 py-2 ">
-                    {fromLocation}
-                  </p>
-                </button>
-                <p className="text-center text-xs font-audimat uppercase">Origin</p>
-              </div>
-              <MoveHorizontalIcon
-                strokeWidth={1}
-                width={50}
-                size={50}
-                className="stroke-ldblue mr-8"
+        <div className="flex flex-row my-20 mx-auto space-x-20 items-center">
+          <motion.div
+            initial={{ scale: 0.25, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 0.25 }}
+            className="flex"
+          >
+            <div className="relative">
+              <button
+                onClick={() => {
+                  setActiveField("from");
+                  setShowSearch(true);
+                }}
+              >
+                <p className="text-3xl md:text-4xl xl:text-6xl font-audimat px-4 py-2 ">
+                  {fromLocation}
+                </p>
+              </button>
+              <p className="text-center text-xs font-audimat uppercase">
+                Origin
+              </p>
+            </div>
+            <MoveHorizontalIcon
+              strokeWidth={1}
+              width={50}
+              size={50}
+              className="stroke-ldblue mr-8"
+            />
+            <div className="relative">
+              <button
+                onClick={() => {
+                  setActiveField("to");
+                  setShowSearch(true);
+                }}
+              >
+                <p className="text-3xl md:text-4xl xl:text-6xl font-audimat  px-4   py-2 ">
+                  {toLocation}
+                </p>
+              </button>
+              <p className="text-center text-xs font-audimat uppercase">
+                Destination
+              </p>
+            </div>
+            {showSearch && activeField && (
+              <AirportPicker
+                setToLocation={setToLocation}
+                setFromLocation={setFromLocation}
+                setShowSearch={setShowSearch}
+                activeField={activeField}
+                toLocation={toLocation}
+                fromLocation={fromLocation}
               />
-              <div className="relative">
-                <button
-                  onClick={() => {
-                    setActiveField("to");
-                    setShowSearch(true);
-                  }}
-                >
-                  <p className="text-3xl md:text-4xl xl:text-6xl font-audimat  px-4   py-2 ">
-                    {toLocation}
-                  </p>
-                </button>
-                <p className="text-center text-xs font-audimat uppercase">Destination</p>
-              </div>
-              {showSearch && activeField && (
-                <AirportPicker
-                  setToLocation={setToLocation}
-                  setFromLocation={setFromLocation}
-                  setShowSearch={setShowSearch}
-                  activeField={activeField}
-                  toLocation={toLocation}
-                  fromLocation={fromLocation}
-                />
-              )}
-            </motion.div>
+            )}
+          </motion.div>
 
-            <div className="">
-              <div className="flex items-center text-xl px-16 font-audimat">
-                <FlightCalendar date={date} setDate={setDate} className="font-audimat" />
-                {/* <div>
+          <div className="">
+            <div className="flex items-center text-xl px-16 font-audimat">
+              <FlightCalendar date={date} setDate={setDate} className="font-audimat" />
+              {/* <div>
                 <Popover>
                   <PopoverTrigger asChild>
                     
@@ -214,21 +217,41 @@ export default function Home() {
                   </PopoverContent>
                 </Popover>
               </div> */}
-                <div className="ml-20">
-                  <motion.button
-                    whileTap={{ scale: 0.5 }}
-                    onClick={() => bookTrip()}
-                    className={` items-center `}
-                  >
-                    <img src="ArrowButton.png" width={75} className="" />
-                  </motion.button>
-                </div>
+              <div className="ml-20">
+                <motion.button
+                  whileTap={{ scale: 0.5 }}
+                  onClick={() => bookTrip()}
+                  className={` items-center `}
+                  
+                >
+                  <img src="ArrowButton.png" width={75} className="" />
+                </motion.button>
               </div>
             </div>
           </div>
         </div>
 
-        <AirlineHero />
+        <div className="relative grid xl:flex xl:py-52 w-full items-center text-white">
+          <img
+            src="dudeguy.png"
+            className="absolute
+             w-full h-full object-cover z-10"
+          />
+
+          <div className="flex flex-col w-1/3 z-20 ml-44">
+            <p className="text-4xl md:text-6xl xl:text-7xl pb-4 font-audimat">
+              Launch Airways
+            </p>
+
+            <p className="textlg: md:text-xl xl:text-2xl font-light pt-4  ">
+              Launch into the skies. In the air in milliseconds, reach your
+              destination without risk, and ship your travel dreams faster than
+              ever before.
+            </p>
+
+            <Button className="bg-pink-600 rounded-none w-1/3 text-3xl px-2 py-8 mt-8">Book Now</Button>
+          </div>
+        </div>
 
         <div className="relative flex flex-col sm:flex-row justify-center gap-x-0 gap-y-6 sm:gap-x-24 py-14 z-0 bg-white !font-sohne ">
           <AirlineInfoCard
