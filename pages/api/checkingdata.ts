41be024beb176fbeab98cd20d4847589ac66eb5a
Cody De Arkland <codydearkland@gmail.com>
2023-11-17 12:38:12 -0800

    fixing ai insights, implementing new ai flag, fixing db migration

--- pages/api/checkingdata.ts
+++ pages/api/checkingdata.ts
@@ -10,24 +10,24 @@ import { checkData } from '@/lib/checkingdata';
 
 
 type Data = {
-    id: number,
-    date: string | null,
-    merchant: string | null,
-    status: string | null,
-    amount: string | null,
-    accounttype: string | null,
-    user: string | null
+  id: number,
+  date: string | null,
+  merchant: string | null,
+  status: string | null,
+  amount: string | null,
+  accounttype: string | null,
+  user: string | null
 }[]
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Data[]>
+  req: NextApiRequest,
+  res: NextApiResponse<Data[]>
 ) {
 
-    const ldClient = await getServerClient(process.env.LD_SDK_KEY || "");
-    const clientContext: any = getCookie('ldcontext', { req, res })
+  const ldClient = await getServerClient(process.env.LD_SDK_KEY || "");
+  const clientContext: any = getCookie('ldcontext', { req, res })
 
-    let dbMigration;
+  let dbMigration;
   let jsonObject
 
   if (clientContext == undefined) {
@@ -42,21 +42,21 @@ export default async function handler(
 
   dbMigration = await ldClient.variation("financialDBMigration", jsonObject, 'off')
 
-    const connectionString = process.env.DB_URL
-    if (!connectionString) {
-        throw new Error('DATABASE_URL is not set')
-    }
-    const client = postgres(connectionString)
-    const db = drizzle(client);
+  const connectionString = process.env.DB_URL
+  if (!connectionString) {
+    throw new Error('DATABASE_URL is not set')
+  }
+  const client = postgres(connectionString)
+  const db = drizzle(client);
 
 
-if (dbMigration === 'complete'){
+  if (dbMigration === 'complete') {
 
     const checkingTransactions = await db.select().from(transactions).where(eq(transactions.accounttype, 'checking'))
     // @ts-ignore
     res.status(200).json(checkingTransactions)
-} else {
-   // @ts-ignore 
-    res.status(200).json(checkData) 
-}
+  } else {
+    // @ts-ignore 
+    res.status(200).json(checkData)
+  }
 }
--- pages/api/creditdata.ts
+++ pages/api/creditdata.ts
@@ -4,6 +4,9 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import { eq } from 'drizzle-orm';
 import postgres from 'postgres'
 import { transactions } from '@/schema/schema'
+import { creditData } from '@/lib/creditInserts';
+import { getServerClient } from '@/utils/ld-server';
+import { getCookie } from 'cookies-next';
 
 type Data = {
     id: number,
@@ -19,6 +22,25 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data[]>
 ) {
+
+    const ldClient = await getServerClient(process.env.LD_SDK_KEY || "");
+    const clientContext: any = getCookie('ldcontext', { req, res })
+
+    let dbMigration;
+    let jsonObject
+
+    if (clientContext == undefined) {
+        jsonObject = {
+            key: '12234',
+            user: "Anonymous"
+        }
+    } else {
+        const json = decodeURIComponent(clientContext);
+        jsonObject = JSON.parse(json);
+    }
+
+    dbMigration = await ldClient.variation("financialDBMigration", jsonObject, 'off')
+
     const connectionString = process.env.DB_URL
     if (!connectionString) {
         throw new Error('DATABASE_URL is not set')
@@ -26,6 +48,14 @@ export default async function handler(
     const client = postgres(connectionString)
     const db = drizzle(client);
 
-    const creditTransactions = await db.select().from(transactions).where(eq(transactions.accounttype, 'credit'))
-    res.status(200).json(creditTransactions)
+
+    if (dbMigration === 'complete') {
+
+        const creditTransactions = await db.select().from(transactions).where(eq(transactions.accounttype, 'credit'))
+        // @ts-ignore 
+        res.status(200).json(creditTransactions)
+    } else {
+        // @ts-ignore 
+        res.status(200).json(creditData)
+    }
 }
