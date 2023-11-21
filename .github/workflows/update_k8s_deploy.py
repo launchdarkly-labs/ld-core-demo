9ac347e966bbdb31f332ef5ef4b12ac9a235f874
Ahmed <ahmedhqadri@gmail.com>
2023-11-21 11:28:12 -0800

    troubleshooting 10

--- .github/workflows/update_k8s_deploy.py
+++ .github/workflows/update_k8s_deploy.py
@@ -6,35 +6,35 @@ from ruamel.yaml import YAML
 import yaml
 import base64
 import time
+import subprocess
 
 def main():
 
     update_deploy_files()
+    
+def replace_placeholders(file_path, replacements):
+    sed_command = ["sed", "-i", "-e"]
+    for placeholder, replacement in replacements.items():
+        sed_command.extend(["s/{}/{}/g".format(placeholder, replacement)])
+    sed_command.append(file_path)
+    
+    subprocess.run(sed_command)
 
 def update_deploy_files():
-
-	create_folder = os.mkdir("./.github/workflows/deploy_files")
 
 	namespace = os.getenv('NAMESPACE')
 	url = "{0}.launchdarklydemos.com".format(namespace)
 	image_url = "955116512041.dkr.ecr.us-east-1.amazonaws.com/ld-core-demo:{0}".format(os.getenv('IMAGE_TAG'))
-
-	input_file = "./.github/workflows/deploy.yaml"
-	output_file = "./.github/workflows/deploy_files/{0}-deploy.yaml".format(namespace)
-	with open(input_file, "r") as f_in:
-		with open(output_file, "w") as f_out:
-			#Replace placeholders with values
-			for line in f_in:
-				if "SERVICE_NAME" in line:
-					line = '  name: {0} \n'.format(namespace)
-				if "HOST_URL" in line: 
-					line = '    - host: {0} \n'.format(url)
-				if "SERVICE_NAME_FOR_INGRESS" in line:
-					line = '                    name: {0} \n'.format(namespace)
-				if "IMAGE_URL" in line:
-					line = '          image: {0} \n'.format(image_url)
-			    
-				f_out.write(line)
+	# Example usage
+	file_path = "./.github/workflows/deploy_files/deploy.yaml"
+	replacements = {
+		"SERVICE_NAME": "{0}".format(namespace),
+		"HOST_URL": "{0}".format(url),
+		"SERVICE_NAME_FOR_INGRESS": "{0}".format(namespace),
+		"IMAGE_URL": "{0}".format(image_url)
+	}
+        
+	replace_placeholders(file_path, replacements)
 
 if __name__ == "__main__":
 	main()
