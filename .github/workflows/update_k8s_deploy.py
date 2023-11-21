53ba90ccb9aa1693722efc7e71ba2b60423ba287
Ahmed <ahmedhqadri@gmail.com>
2023-11-21 11:56:07 -0800

    troubleshooting 12

--- .github/workflows/update_k8s_deploy.py
+++ .github/workflows/update_k8s_deploy.py
@@ -11,30 +11,24 @@ import subprocess
 def main():
 
     update_deploy_files()
-    
-def replace_placeholders(file_path, replacements):
-    sed_command = ["sed", "-i", "-e"]
-    for placeholder, replacement in replacements.items():
-        sed_command.extend(["s/{}/{}/g".format(placeholder, replacement)])
-    sed_command.append(file_path)
-    
-    subprocess.run(sed_command)
 
 def update_deploy_files():
 
 	namespace = os.getenv('NAMESPACE')
 	url = "{0}.launchdarklydemos.com".format(namespace)
 	image_url = "955116512041.dkr.ecr.us-east-1.amazonaws.com/ld-core-demo:{0}".format(os.getenv('IMAGE_TAG'))
-	# Example usage
-	file_path = "./.github/workflows/deploy_files/deploy.yaml"
-	replacements = {
-		"placeholder1": "{0}".format(namespace),
-		"placeholder2": "{0}".format(url),
-		"placeholder3": "{0}".format(namespace),
-		"placeholder4": "{0}".format(image_url)
-	}
+
+	sed_command = ["sed -i 's|placeholder1|{0}|g' ./.github/workflows/deploy_files/deploy.yaml".format(namespace)]
+	subprocess.run(sed_command, shell=True)
+    
+	sed_command = ["sed -i 's|placeholder2|{0}|g' ./.github/workflows/deploy_files/deploy.yaml".format(url)]
+	subprocess.run(sed_command, shell=True)
         
-	replace_placeholders(file_path, replacements)
+	sed_command = ["sed -i 's|placeholder3|{0}|g' ./.github/workflows/deploy_files/deploy.yaml".format(namespace)]
+	subprocess.run(sed_command, shell=True)
+        
+	sed_command = ["sed -i 's|placeholder4|{0}|g' ./.github/workflows/deploy_files/deploy.yaml".format(image_url)]
+	subprocess.run(sed_command, shell=True)	
 
 if __name__ == "__main__":
 	main()
