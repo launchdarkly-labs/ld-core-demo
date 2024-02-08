d9d4f83a842956a1642ff28be88f0f6e84c20301
Ahmed Qadri <ahmedhqadri@gmail.com>
2024-02-08 07:04:48 +0000

    debugging

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default {
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {
-    connectionString: process.env.DB_URL || "postgresql://postgres:LDD3v3xp@fielddemodb.cii3vie7naoe.us-west-2.rds.amazonaws.com:5432",
+    connectionString: process.env.DB_URL || "",
   }
 } satisfies Config;
