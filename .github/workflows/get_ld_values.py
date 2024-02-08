1411b47a1a5c3544dfa223696018986735c58133
Ahmed <ahmedhqadri@gmail.com>
2024-02-08 12:43:53 -0800

    update ld-booth deployment

--- .github/workflows/get_ld_values.py
+++ .github/workflows/get_ld_values.py
@@ -16,12 +16,12 @@ def getLDEnvs():
 
     LD_API_KEY = os.getenv('LD_API_KEY')
     LD_ENV_KEY = os.getenv('LD_ENV_KEY')
-    
+
     if LD_API_KEY is None:
         print("LD_API_KEY not set")
         exit(1)
 
-   
+
     url = "https://app.launchdarkly.com/api/v2/projects/ld-core-demo/environments/" + LD_ENV_KEY
 
     headers = {"Authorization": LD_API_KEY}
