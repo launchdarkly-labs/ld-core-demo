1afb8c0af6d7034e3fec1680c6638aa97150386a
Ahmed <ahmedhqadri@gmail.com>
2024-02-13 16:38:06 -0800

    Added Device / OS detection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import "@/styles/bank.css";
+import { isAndroid, isIOS, isBrowser, isMobile, isMacOs, isWindows } from 'react-device-detect';
 import "@/styles/home.css";
 import type { AppProps } from "next/app";
 import NoSSRWrapper from "@/components/no-ssr";
@@ -15,6 +16,9 @@ let c;
 if (typeof window !== "undefined") {
   const uniqueKey = uuidv4().slice(0, 4);
 
+  const operatingSystem = isAndroid ? 'Android' : isIOS ? 'iOS' : isWindows ? 'Windows' : isMacOs ? 'macOS' : '';
+  const device = isMobile ? 'Mobile' : isBrowser ? 'Desktop' : '';
+
   const LDProvider = await asyncWithLDProvider({
     clientSideID: process.env.NEXT_PUBLIC_LD_CLIENT_KEY || "",
     reactOptions: {
@@ -29,8 +33,8 @@ if (typeof window !== "undefined") {
       },
       device: {
         key: uniqueKey,
-        operating_system: "MacOS",
-        mobile_device: "False",
+        operating_system: operatingSystem,
+        platform: device,
       },
       location: {
         key: uniqueKey,
@@ -49,18 +53,18 @@ if (typeof window !== "undefined") {
       <NoSSRWrapper>
         <LDProvider>
           <PersonaProvider>
-          <LoginProvider>
-            <TripsProvider>
-              <KeyboardNavigation />
-              <Head>
-                <meta
-                  name="viewport"
-                  content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
-                />
-              </Head>
-              <Component {...pageProps} />
-            </TripsProvider>
-          </LoginProvider>
+            <LoginProvider>
+              <TripsProvider>
+                <KeyboardNavigation />
+                <Head>
+                  <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+                  />
+                </Head>
+                <Component {...pageProps} />
+              </TripsProvider>
+            </LoginProvider>
           </PersonaProvider>
         </LDProvider>
       </NoSSRWrapper>
