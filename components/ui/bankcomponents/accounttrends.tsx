efee422e5923e07d791e7099f58e320f5001e820
Tony <tony.w.trinh3@gmail.com>
2024-02-21 14:11:25 -0500

    added set height to graph

--- components/ui/bankcomponents/accounttrends.tsx
+++ components/ui/bankcomponents/accounttrends.tsx
@@ -6,7 +6,7 @@ export function AccountTrends({data}) {
 
     return (
 
-        <div className={`flex flex-col p-10 shadow-xl w-full h-full xl:h-[292px] bg-white justify-center sm:col-span-1 lg:col-span-2 `}>
+        <div className={`flex flex-col p-10 shadow-xl w-full h-[425px] lg:h-[425px] xl:h-[425px] bg-white justify-center sm:col-span-1 lg:col-span-2 `}>
               <p className="aiinsightstext pt-4">
                 6-Month Account Trend
               </p>
