5a907a43a71f4ad09c180d3c6237bea298a38537
Tony <tony.w.trinh3@gmail.com>
2024-03-29 14:54:39 -0400

    change toast time

--- components/ui/use-toast.ts
+++ components/ui/use-toast.ts
@@ -6,8 +6,8 @@ import type {
   ToastProps,
 } from "@/components/ui/toast"
 
-const TOAST_LIMIT = 1
-const TOAST_REMOVE_DELAY = 1000000
+const TOAST_LIMIT = 1;
+const TOAST_REMOVE_DELAY = 100;
 
 type ToasterToast = ToastProps & {
   id: string
