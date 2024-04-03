e33cadddd6d9c9b2cdc0f82c95322dd7cbf9e120
Tony <tony.w.trinh3@gmail.com>
2024-04-03 15:31:24 -0400

    still working on fixing launchclub box

--- components/ui/airwayscomponents/airlineHero.tsx
+++ components/ui/airwayscomponents/airlineHero.tsx
@@ -18,9 +18,9 @@ const AirlineHero = ({
       <div className="flex flex-col">
         <div className={`relative`}>
           <div aria-hidden="true" className="absolute hidden h-full w-1/2 lg:block" />
-          <div className="relative bg-transparent">
+          <div className="relative lg:bg-transparent">
             <div className={`mx-auto max-w-7xl px-4 sm:px-6 grid lg:grid-cols-2 lg:px-8 `}>
-              <div className=" max-w-2xl py-[5rem] lg:max-w-none lg:py-32 z-10 lg:pr-10">
+              <div className=" mx-auto max-w-2xl py-[5rem] lg:max-w-none lg:py-32 z-10 lg:pr-10">
                 <div className="flex flex-row">
                   <div className={`flex flex-col`}>
                     <h1 className={`text-4xl md:text-6xl xl:text-7xl pb-4 font-audimat`}>
@@ -38,28 +38,31 @@ const AirlineHero = ({
                   </div>
                 </div>
               </div>
-              {launchClubLoyalty && isLoggedIn && !enrolledInLaunchClub && (
-                <motion.div
-                  initial={{ x: 300 }}
-                  animate={{ x: 0 }}
-                  transition={{ type: "spring", stiffness: 50 }}
-                  className="flex flex-col p-10 gap-y-8 z-30  w-full h-auto sm:h-[300px] sm:w-[500px]
-                   bg-gradient-airline shadow-2xl mb-[5rem]  lg:my-32 lg:absolute lg:right-10 rounded-lg text-white"
-                >
-                  <div className="text-center">
-                    <h3 className="text-4xl text-center mb-4">Are you ready to Launch?!</h3>
-                    <p className="text-xl ">
-                      Join Launch Club for exclusive access to flights, rewards, and much more. See
-                      details within!
-                    </p>
-                  </div>
-                  <div className="h-full flex self-center">
-                    <LaunchSignUp />
-                  </div>
-                </motion.div>
-              )}
             </div>
           </div>
+          <div className="h-48 w-full sm:h-64 lg:absolute lg:top-0 lg:right-0 lg:h-full lg:w-1/2 flex justify-center">
+            {launchClubLoyalty && isLoggedIn && !enrolledInLaunchClub && (
+              <motion.div
+                initial={{ x: 300 }}
+                animate={{ x: 0 }}
+                transition={{ type: "spring", stiffness: 50 }}
+                className="flex flex-col p-10 gap-y-8 z-30  w-full h-auto sm:h-[300px] sm:w-[500px]
+                bg-gradient-airline shadow-2xl mb-[5rem] lg:my-32 relative rounded-lg text-white"
+              >
+                <div className="text-center">
+                  <h3 className="text-4xl text-center mb-4">Are you ready to Launch?!</h3>
+                  <p className="text-xl ">
+                    Join Launch Club for exclusive access to flights, rewards, and much more. See
+                    details within!
+                  </p>
+                </div>
+                <div className="h-full flex self-center">
+                  <LaunchSignUp />
+                </div>
+              </motion.div>
+            )}
+          </div>
+
           <div className={`w-full h-full absolute top-0 lg:h-full lg:w-full`}>
             <img
               src={"dudeguy.png"}
