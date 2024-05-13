9ff2a2293aecd39b93015fb2c605aac999eeef00
Tony <tony.w.trinh3@gmail.com>
2024-05-13 12:29:52 -0400

    fix news

--- components/ui/investmentcomponents/NewsCard.tsx
+++ components/ui/investmentcomponents/NewsCard.tsx
@@ -4,14 +4,7 @@ import React, { useEffect, useState } from "react";
 import { BounceLoader } from "react-spinners";
 import { truncateString } from "@/utils/utils";
 import { wait, randomLatency } from "@/utils/utils";
-import {
-  newsData,
-  investmentData,
-  stockData,
-} from "@/components/ui/investmentcomponents/InvestmentData";
-
-const time = new Date().getTime();
-const date = format(new Date(time), "MMM d, yyyy");
+import { newsData } from "@/components/ui/investmentcomponents/InvestmentData";
 
 const NewsCard = () => {
   const [loadingStocksTable, setStocksTable] = useState(false);
@@ -35,25 +28,24 @@ const NewsCard = () => {
         </div>
       ) : (
         <div className="flex flex-col gap-y-2 mb-4">
-          {newsData?.splice(0,7).map((datum: any, index: number) => {
+          {newsData?.slice(0, 7).map((datum: any, index: number) => {
             const dateCleaned = format(new Date(datum.published_utc), "MMM d, yyyy");
             return (
               <div className="" key={`${datum.id}-${index}`}>
-                <a
+                <p
                   className="text-sm hover:underline cursor-pointer"
-                  href={datum.article_url}
                   data-testid="stock-news-title-test-id"
                   title={datum.title}
                 >
                   {truncateString(datum.title, 60)}
-                </a>
+                </p>
                 <p className="text-investmentgrey text-xs">
                   {datum.publisher.name} &#183; <span>{dateCleaned}</span>
                 </p>
               </div>
             );
           })}
-          <p className="text-investmentblue hover:underline cursor-default text-center">
+          <p className="text-investmentblue hover:underline cursor-default text-center mt-4">
             View More
           </p>
         </div>
