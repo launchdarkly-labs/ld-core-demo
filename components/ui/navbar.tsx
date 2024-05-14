65cc15d1c8ee596dc5ce9e087af7749572c3b437
Tony <tony.w.trinh3@gmail.com>
2024-05-14 15:45:57 -0400

    login in popup nav

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -27,6 +27,7 @@ import toggleBankHorizontalLogo from "@/public/banking/toggleBank_logo_horizonta
 import frontierCapitalHorizontalLogo from "@/public/investment/frontier_capital_logo_horitzonal.svg";
 import launchAirwaysHorizontalLogo from "@/public/airline/launch_airways_logo_horizontal.svg";
 import galaxyMarketplaceHorizontalLogo from "@/public/marketplace/galaxy_marketplace_logo_horizontal.svg";
+import { LoginComponent } from "./logincomponent";
 
 const variantToImageMap = {
   bank: toggleBankHorizontalLogo.src,
@@ -52,7 +53,9 @@ interface Persona {
 
 const NavBar = React.forwardRef<any, NavBarProps>(
   ({ launchClubLoyalty, cart, setCart, className, variant, handleLogout, ...props }, ref) => {
-    const { isLoggedIn, enrolledInLaunchClub, user, loginUser } = useContext(LoginContext);
+    const { isLoggedIn, enrolledInLaunchClub, user, loginUser, setIsLoggedIn } =
+      useContext(LoginContext);
+
     let navChild, navLinkMobileDropdown, navLinksGroup;
     const navLinkStyling =
       "hidden sm:block bg-transparent pb-[3rem] flex items-start text-base font-sohnelight font-medium transition-colors bg-no-repeat bg-bottom bg-[length:100%_3px] cursor-auto";
@@ -303,37 +306,55 @@ const NavBar = React.forwardRef<any, NavBarProps>(
               <PopoverTrigger>
                 <Avatar>
                   <AvatarImage
-                    src={chosenPersona?.personaimage || "ToggleAvatar.png"}
+                    src={
+                      personas.find((persona) => persona.personaname === user)?.personaimage ||
+                      "ToggleAvatar.png"
+                    }
                     className=""
                   />
                 </Avatar>
               </PopoverTrigger>
-              <PopoverContent className="w-[300px] h-[440px]">
-                <>
-                  <div className="mx-auto flex place-content-center w-full">
-                    <img
-                      src={chosenPersona?.personaimage || "ToggleAvatar.png"}
-                      className="rounded-full h-48"
-                    />
-                  </div>
-                  <div className="mx-auto text-center  align-center flex text-black font-sohnelight pt-4  text-xl items-center align-center">
-                    <p className="pt-4">
-                      Thank you {chosenPersona?.personaname || user} for shopping with us as
-                      {"  "}
-                      <br></br>
-                      <span className="text-2xl">Premium Member</span>!
-                    </p>
-                  </div>
-                  <div className="mx-auto text-center">
-                    <Button
-                      onClick={handleLogout}
-                      className=" bg-red-700 items-center font-audimat my-2 w-full  bg-gradient-experimentation  text-xl rounded-none"
-                    >
-                      Logout
-                    </Button>
-                    <QuickLoginDialog personas={personas} variant={variant} />
-                  </div>
-                </>
+              <PopoverContent className={`w-[300px] h-auto ${!isLoggedIn ? "p-0" : ""}`}>
+                {isLoggedIn ? (
+                  <>
+                    <div className="mx-auto flex place-content-center w-full">
+                      <img
+                        src={
+                          personas.find((persona) => persona.personaname === user)?.personaimage ||
+                          "ToggleAvatar.png"
+                        }
+                        className="rounded-full h-48"
+                      />
+                    </div>
+                    <div className="mx-auto text-center">
+                      <p className="text-2xl font-normal text-black font-shone mt-4">
+                        Hi {chosenPersona?.personaname}
+                      </p>
+                    </div>
+                    <div className="mx-auto text-center">
+                      <p className="text-md uppercase font-normal tracking-widest text-[#939598] font-shone mt-0">
+                        PLATINUM MEMBER
+                      </p>
+                    </div>
+                    <div className="mx-auto text-center mt-4">
+                      <Button
+                        onClick={handleLogout}
+                        className="items-center hover:bg-gradient-experimentation-grey 
+                       hover:text-white font-audimat my-2 w-full bg-gradient-experimentation text-white text-xl rounded-none"
+                      >
+                        Logout
+                      </Button>
+                      <QuickLoginDialog personas={personas} />
+                    </div>
+                  </>
+                ) : (
+                  <LoginComponent
+                    isLoggedIn={isLoggedIn}
+                    setIsLoggedIn={setIsLoggedIn}
+                    loginUser={loginUser}
+                    name={name}
+                  />
+                )}
               </PopoverContent>
             </Popover>
           </div>
