f91b41cfec52201304efb939ad506dd424f93217
Ahmed <ahmedhqadri@gmail.com>
2024-06-04 13:14:51 -0700

    fix_update_issue

--- pages/api/bedrock.ts
+++ pages/api/bedrock.ts
@@ -9,16 +9,6 @@ export default async function bedrockCall(req: NextApiRequest, res: NextApiRespo
     const client = new BedrockRuntimeClient({ region: "us-west-2" });
     const prompt = req.body;
 
-
-// Debug jurassic another time 
-    const input2 = {
-        modelId: "ai21.j2-ultra-v1",
-        contentType: "application/json",
-        accept: "application/json",
-        body: JSON.stringify({"prompt":`\n${prompt}`,"maxTokens":200,"temperature":0.7,"topP":1,"stopSequences":[],"countPenalty":{"scale":0},"presencePenalty":{"scale":0},"frequencyPenalty":{"scale":0}})
-    }
-//
-
     
     const command = new InvokeModelCommand(input);
     try {
