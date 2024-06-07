f3fbe3b3672ede3b6ca878ee5ab75b81700ecde5
Peter McCarron <pcmccarron@gmail.com>
2024-06-07 11:09:42 -0600

    switched to converse API

--- pages/api/destination-picker.ts
+++ pages/api/destination-picker.ts
@@ -1,6 +1,7 @@
 import {
     BedrockRuntimeClient,
-    InvokeModelCommand,
+    ConverseCommand,
+    ConversationRole,
 } from "@aws-sdk/client-bedrock-runtime";
 import { NextApiRequest, NextApiResponse } from 'next';
 import { ldClient } from "@/utils/ld-server/serverClient";
@@ -23,62 +24,31 @@ export default async function bedrockCall(req: NextApiRequest, res: NextApiRespo
         "modelId": "anthropic.claude-3-haiku-20240307-v1:0",
         "name": "claude-haiku"
     })
-    let input = {
-        modelId: "",
-        contentType: "",
-        accept: "",
-        body: "",
-    }
-    // Add models here
-    if (model?.name === 'claude-haiku') {
-    input = {
-        modelId: model?.modelId,
-        contentType: "application/json",
-        accept: "application/json",
-        body: JSON.stringify({
-            anthropic_version: model?.anthropic_version,
-            max_tokens: model?.max_tokens,
-            messages: [
-                {
-                    role: "user",
-                    content: [
-                        {
-                            type: "text",
-                            text: prompt,
-                        },
-                    ],
-                },
-            ],
-        }),
-    }
-}
-if (model?.name === "cohere-text") {
-    console.log(model?.output)
-    input = {
-        modelId: model?.modelId,
-        contentType: "application/json",
-        accept: "*/*",
-        body: JSON.stringify({
-            prompt: prompt,
-            max_tokens: model?.max_tokens,
-            temperature: model?.temperature,
-            p: 0.01,
-            k: 0,
-            stop_sequences: [],
-            return_likelihoods: "NONE",
-        }),
-    };
-}
-    //
 
-    const command = new InvokeModelCommand(input);
+const messages = [
+    {
+        role: "user" as ConversationRole,
+        content: [
+            {
+                text: prompt
+            }
+        ]
+    }
+    ]
+
+    const command = new ConverseCommand({
+        modelId: model?.modelId, 
+        messages: messages,
+        inferenceConfig: {
+                maxTokens: model?.max_tokens,
+                temperature: model?.temperature || 0
+            }});
     try {
         const response = await client.send(command);
-        
-        let decoder = new TextDecoder();
-        let jsontext = await JSON.parse(decoder.decode(response.body));
-        console.log(jsontext[model?.output][0].text)
-        res.status(200).json(jsontext[model?.output][0].text);
+        const responseText = response?.output?.message?.content?.[0]?.text || ""
+        console.log(responseText)
+        return res.status(200).json(responseText)
+
     } catch (error: any) {
 
         throw new Error(error.message);
