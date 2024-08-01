3feab5b894e4609956ad8afe13b7f98c9bd49278
Tony <tony.w.trinh3@gmail.com>
2024-08-01 14:51:58 -0400

    simplify suggesteditems

--- components/ui/marketcomponents/suggestedItems.tsx
+++ components/ui/marketcomponents/suggestedItems.tsx
@@ -1,20 +1,8 @@
-
-import { useEffect, useState } from "react";
-import vrgame from "@/public/marketplace/vrgalaxy_image/vrgame.svg";
-import vrcamera from "@/public/marketplace/vrgalaxy_image/vrcamera.svg";
-import vrheadset from "@/public/marketplace/vrgalaxy_image/vrheadset.svg";
-import vrsoftware from "@/public/marketplace/vrgalaxy_image/vrsoftware.svg";
-import vrtreadmill from "@/public/marketplace/vrgalaxy_image/vrtreadmill.svg";
-import hapticgloves from "@/public/marketplace/vrgalaxy_image/hapticgloves.svg";
-import vrheadsetcleaningkit from "@/public/marketplace/vrgalaxy_image/vrheadsetcleaningkit.svg";
-import vrcontrollers from "@/public/marketplace/vrgalaxy_image/vrcontrollers.svg";
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import galaxyMarketLogo from '@/public/market.png'
 import { useLDClient } from "launchdarkly-react-client-sdk";
-
-
-
+import { VR_GALAXY_DATA } from "@/utils/constants";
 interface InventoryItem {
     id: number;
     vendor: string;
@@ -23,11 +11,9 @@ interface InventoryItem {
     image?: StaticImageData;
 }
 
-
-
 export default function SuggestedItems({ cart, setCart }: { cart: any, setCart: any }) {
 
-    const [inventory, setInventory] = useState<InventoryItem[]>([]);
+
     const LDClient = useLDClient();
 
 
@@ -41,45 +27,18 @@ export default function SuggestedItems({ cart, setCart }: { cart: any, setCart: 
         LDClient?.track("upsell-tracking", LDClient.getContext());
     }
 
-    useEffect(() => {
-        const data = [
-            {
-                "id": 6,
-                "vendor": "vrgalaxy",
-                "item": "VR Headset Cleaning Kit",
-                "cost": "29.99",
-                "image": vrheadsetcleaningkit
-            },
-            {
-                "id": 7,
-                "vendor": "vrgalaxy",
-                "item": "360° VR Camera",
-                "cost": "349.99",
-                "image": vrcamera
-            },
-            {
-                "id": 8,
-                "vendor": "vrgalaxy",
-                "item": "Virtual Reality Development Software",
-                "cost": "199.99",
-                "image": vrsoftware
-            },
-        ];
-        setInventory(data);
-    }, []);
-
     return (
-        <div className="flex flex-col items-center justify-center w-full h-full ">
-            <div className="flex flex-col items-center font-sohne bg-gradient-experimentation text-transparent bg-clip-text">
+        <div className="flex flex-col items-center justify-center w-full h-full my-4  font-sohne ">
+            <h3 className="flex flex-col items-center bg-gradient-experimentation text-transparent bg-clip-text my-4">
                 Suggested Items
-            </div>
-            <Carousel showArrows={true} autoPlay={true} infiniteLoop={true}>
-                {inventory.map(item => (
+            </h3>
+            <Carousel showArrows={true} autoPlay={true} infiniteLoop={true} className="w-full h-full">
+                {VR_GALAXY_DATA.slice(5, 8).map(item => (
                     <div key={item.id} className="flex flex-col items-center">
                         <img src={`${item.image ? item.image?.src : galaxyMarketLogo.src}`} alt={item.item} />
                         <div className="flex flex-col items-center ">
                             <p className="legend mb-12">{item.item} - ${item.cost}</p>
-                            <button className="legend " onClick={() => addedSuggestedItemToCart(item)}>Add To Cart</button>
+                            <button className="legend  !bg-gradient-experimentation" onClick={() => addedSuggestedItemToCart(item)}>Add To Cart</button>
                         </div>
                     </div>
                 ))}
