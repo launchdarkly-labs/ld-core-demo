9f86b6613c0dd6a7702ddb899a39753a51741db8
Tony <tony.w.trinh3@gmail.com>
2024-08-02 17:26:02 -0400

    created experiments in py

--- .github/workflows/ld_api/create_experiment.py
+++ .github/workflows/ld_api/create_experiment.py
@@ -13,6 +13,8 @@ def main():
     createFunnelExperiment()
     createFeatureExperiment()
     createAIFeatureExperiment()
+    createSearchEngineIncreaseAddToCartFeatureExperiment()
+    createShortenCollectionsPageIncreaseConversionFunnelExperiment()
 
 def createAIFeatureExperiment():
     
@@ -273,6 +275,131 @@ def createFeatureExperiment():
             print(response.status_code)
             break
 
+def getearchEngineIncreaseAddToCartFeatureFlagDetails():
+    
+    ld_api_key = os.getenv('LD_API_KEY')
+    namespace = os.getenv('NAMESPACE')
+    project_key = f"{namespace}-ld-demo"
+    flag_key = "release-new-search-engine"
+    
+    if not ld_api_key:
+        print("LD_API_KEY not set")
+        exit(1)
+    
+    if not namespace:
+        print("NAMESPACE not set")
+        exit(1)
+        
+    url = "https://app.launchdarkly.com/api/v2/flags/" + project_key + "/" + flag_key
+    
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": ld_api_key,
+    }
+    while True:
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            print("Flag details retrieved successfully")
+            return response.json().get("variations")
+        elif response.status_code == 429:
+            print("Rate limit exceeded, waiting 10 seconds to retry...")
+            time.sleep(5)
+        else:
+            print("Failed to retrieve flag details")
+            exit(1)
+
+def createSearchEngineIncreaseAddToCartFeatureExperiment():
+    
+    ld_api_key = os.getenv('LD_API_KEY')
+    namespace = os.getenv('NAMESPACE')
+    project_key = f"{namespace}-ld-demo"
+
+    if not ld_api_key:
+        print("LD_API_KEY not set")
+        exit(1)
+        
+    if not namespace:
+        print("NAMESPACE not set")
+        exit(1)
+        
+    variations = getearchEngineIncreaseAddToCartFeatureFlagDetails()
+        
+    url = "https://app.launchdarkly.com/api/v2/projects/" + project_key + "/environments/" + namespace + "/experiments"
+    
+    payload = {
+        "name": "Search Engine Increase Add To Cart",
+        "description": "See if new precise search engine with direct add to cart button lead to increase in cart prices",
+        "maintainerId": "6127d90d9971632664df6f1a",
+        "key": "search-engine-add-to-cart-experiment",
+        "iteration": {
+            "hypothesis": "If we enable the new search engine feature, we can drive greater upsell conversion.",
+            "canReshuffleTraffic": True,
+            "metrics": [
+            {
+                "key": "search-engine-add-to-cart",
+                "isGroup": False,
+            },
+            {
+                "key": "in-cart-total-price",
+                "isGroup": False,
+            }
+            ],
+            "primarySingleMetricKey": "search-engine-add-to-cart",
+            "treatments": [
+                {
+                    "name": variations[0]['name'],
+                    "baseline": True,
+                    "allocationPercent": "50",
+                    "parameters": [
+                    {
+                        "flagKey": "release-new-search-engine",
+                        "variationId": variations[0]['_id']
+                    }
+                    ]
+                },
+                {
+                    "name": variations[1]['name'],
+                    "allocationPercent": "50",
+                    "parameters": [
+                    {
+                        "flagKey": "release-new-search-engine",
+                        "variationId": variations[1]['_id']
+                    }
+                    ]
+                },
+            ],
+            "flags": {
+            "release-new-search-engine": {
+                "ruleId": "fallthrough",
+                "flagConfigVersion": 1
+            },
+            },
+            "randomizationUnit": "audience"
+        }
+    }
+    
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": ld_api_key,
+        "LD-API-Version": "beta"
+    }
+    
+    response = requests.post(url, json=payload, headers=headers)
+
+    while True:
+        if response.status_code == 201:
+            print("Search Engine Increase Add To Cart Feature Experiment created successfully")
+            break
+        elif response.status_code == 429:
+            print("Rate limit exceeded, waiting 10 seconds to retry...")
+            time.sleep(10)
+        else:
+            data = response.json()
+            print(data)
+            print(response.status_code)
+            break
+
 def getFunnelFeatureFlagDetails():
         
         ld_api_key = os.getenv('LD_API_KEY')
@@ -306,8 +433,7 @@ def getFunnelFeatureFlagDetails():
                 time.sleep(5)
             else:
                 print("Failed to retrieve flag details")
-                exit(1)
-            
+                exit(1)           
             
 def createFunnelExperiment():
     
@@ -401,6 +527,132 @@ def createFunnelExperiment():
     while True:
         if response.status_code == 201:
             print("Store Headers Funnel Experiment created successfully")
+            break
+        elif response.status_code == 429:
+            print("Rate limit exceeded, waiting 10 seconds to retry...")
+            time.sleep(10)
+        else:
+            data = response.json()
+            print(data)
+            print(response.status_code)
+            break
+
+def getShortenCollectionsPageIncreaseConversionFunnelFeatureFlagDetails():
+        
+        ld_api_key = os.getenv('LD_API_KEY')
+        namespace = os.getenv('NAMESPACE')
+        project_key = f"{namespace}-ld-demo"
+        flag_key = "release-new-shorten-collections-page"
+        
+        if not ld_api_key:
+            print("LD_API_KEY not set")
+            exit(1)
+        
+        if not namespace:
+            print("NAMESPACE not set")
+            exit(1)
+            
+        url = "https://app.launchdarkly.com/api/v2/flags/" + project_key + "/" + flag_key
+        
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": ld_api_key,
+        }
+        while True:
+            response = requests.get(url, headers=headers)
+
+            
+            if response.status_code == 200:
+                print("Flag details retrieved successfully")
+                return response.json().get("variations")
+            elif response.status_code == 429:
+                print("Rate limit exceeded, waiting 10 seconds to retry...")
+                time.sleep(5)
+            else:
+                print("Failed to retrieve flag details")
+                exit(1)           
+            
+def createShortenCollectionsPageIncreaseConversionFunnelExperiment():
+    
+    ld_api_key = os.getenv('LD_API_KEY')
+    namespace = os.getenv('NAMESPACE')
+    project_key = f"{namespace}-ld-demo"
+
+    if not ld_api_key:
+        print("LD_API_KEY not set")
+        exit(1)
+        
+    if not namespace:
+        print("NAMESPACE not set")
+        exit(1)
+        
+    variations = getShortenCollectionsPageIncreaseConversionFunnelFeatureFlagDetails()
+        
+    url = "https://app.launchdarkly.com/api/v2/projects/" + project_key + "/environments/" + namespace + "/experiments"
+    
+    payload = {
+        "name": "Shorten Collections Page Increase Conversion",
+        "description": "Does shortening the collections page to the top 3 items increase conversion versus showing all of the collection?",
+        "maintainerId": "6127d90d9971632664df6f1a",
+        "key": "shorten-collections-page-increase-conversion-funnel-experiment",
+        "iteration": {
+            "hypothesis": "Does shortening the collections page to the top 3 items increase conversion versus showing all of the collection?",
+            "canReshuffleTraffic": True,
+            "metrics": [
+            {
+                "key": "store-checkout-metrics",
+                "isGroup": True,
+            },
+            {
+                "key": "in-cart-total-price",
+                "isGroup": False,
+            }
+            ],
+            "primaryFunnelKey": "store-checkout-metrics",
+            "treatments": [
+                {
+                    "name": variations[0]['name'],
+                    "baseline": True,
+                    "allocationPercent": "50",
+                    "parameters": [
+                    {
+                        "flagKey": "release-new-shorten-collections-page",
+                        "variationId": variations[0]['_id']
+                    }
+                    ]
+                },
+                {
+                    "name": variations[1]['name'],
+                    "allocationPercent": "50",
+                    "parameters": [
+                    {
+                        "flagKey": "release-new-shorten-collections-page",
+                        "variationId": variations[1]['_id']
+                    }
+                    ]
+                },
+            ],
+            "flags": {
+            "release-new-shorten-collections-page": {
+                "ruleId": "fallthrough",
+                "flagConfigVersion": 1
+            },
+            },
+            "randomizationUnit": "audience"
+        }
+    }
+    
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": ld_api_key,
+        "LD-API-Version": "beta"
+    }
+    
+    response = requests.post(url, json=payload, headers=headers)
+
+    while True:
+        if response.status_code == 201:
+            print("Shorten Collections Page Increase Conversion Funnel Experiment created successfully")
             break
         elif response.status_code == 429:
             print("Rate limit exceeded, waiting 10 seconds to retry...")
