bf16e34e79b2873a915459cb04f17086fbbd8f6a
Ahmed <ahmedhqadri@gmail.com>
2024-11-07 19:08:54 -0800

    updated bank homepage

--- pages/bank.tsx
+++ pages/bank.tsx
@@ -2,19 +2,32 @@ import { useContext } from "react";
 import LoginContext from "@/utils/contexts/login";
 import BankHomePage from "@/components/ui/bankcomponents/bankHomePage";
 import BankUserDashboard from "@/components/ui/bankcomponents/bankUserDashboard";
+import bankHomePageBackgroundRight from "@/public/banking/backgrounds/bank-homepage-background-right.svg";
+import bankHomePageBackgroundLeft from "@/public/banking/backgrounds/bank-homepage-background-left.svg";
+import Image from "next/image";
 
 export default function Bank() {
   const { isLoggedIn } = useContext(LoginContext);
   return (
     <>
-        <div className="bg-bank-homepage-background min-h-screen  ">
-          
+      <Image
+        src={bankHomePageBackgroundRight}
+        className="fixed right-0 bottom-0 m-h-screen"
+        alt="Bank Home Page Background"
+      />
+      <Image
+        src={bankHomePageBackgroundLeft}
+        className="fixed left-0 bottom-0 m-h-screen"
+        alt="Bank Home Page Background"
+      />
+      <div className=" min-h-screen  ">
+
         {!isLoggedIn ? (
-          <BankHomePage/>
+          <BankHomePage />
         ) : (
-          <BankUserDashboard /> 
+          <BankUserDashboard />
         )}
-    </div>
-      </>
-    );    
-  }
+      </div>
+    </>
+  );
+}
