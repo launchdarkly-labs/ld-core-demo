c0b4d8ac2aa2c40c8b6b481f7047770e0fe6044f
Tony <tony.w.trinh3@gmail.com>
2025-01-27 11:52:29 -0500

    updated frequentist funnel

--- experimentation-automation/funnelExperimentGeneratorFunctions.ts
+++ experimentation-automation/funnelExperimentGeneratorFunctions.ts
@@ -9,16 +9,16 @@ const probablityExperimentTypeStoreHeader = {
     ["Final Hours!"]: {
       metric1: 70,
       metric2: 60,
-      metric3: 60,
+      metric3: 50,
       metric4: 20,
     },
     ["Sale"]: {
       metric1: 70,
       metric2: 60,
-      metric3: 50,
+      metric3: 60,
       metric4: 25,
     },
-    ["New Items"]: {
+    ["New Items"]: { //control
       metric1: 70,
       metric2: 60,
       metric3: 30,
@@ -49,47 +49,30 @@ const probablityExperimentTypeStoreHeader = {
 
 const probablityExperimentTypeShortenCollection = {
   ["bayesian"]: {
-    ["Final Hours!"]: {
-      metric1: 70,
-      metric2: 60,
-      metric3: 60,
-      metric4: 20,
-    },
-    ["Sale"]: {
-      metric1: 70,
-      metric2: 60,
-      metric3: 50,
-      metric4: 25,
-    },
-    ["New Items"]: {
+    ["old-long-collections-page"]: {
       metric1: 70,
       metric2: 60,
       metric3: 30,
-      metric4: 20,
+    },
+    ["new-shorten-collections-page"]: {
+      metric1: 50,
+      metric2: 40,
+      metric3: 20,
     },
   },
   ["frequentist"]: {
-    ["Final Hours!"]: {
+    ["old-long-collections-page"]: {
       metric1: 70,
       metric2: 60,
       metric3: 60,
-      metric4: 20,
     },
-    ["Sale"]: {
+    ["new-shorten-collections-page"]: {
       metric1: 70,
       metric2: 60,
       metric3: 50,
-      metric4: 25,
-    },
-    ["New Items"]: {
-      metric1: 70,
-      metric2: 60,
-      metric3: 30,
-      metric4: 20,
     },
   },
 };
-
 
 export const generateStoreHeaderFunnelExperimentResults = async ({
   client,
@@ -126,7 +109,9 @@ export const generateStoreHeaderFunnelExperimentResults = async ({
     let stage1metric = Math.random() * 100;
 
     const metricProbablityObj =
-      probablityExperimentTypeStoreHeader[experimentType as keyof typeof probablityExperimentTypeStoreHeader];
+      probablityExperimentTypeStoreHeader[
+        experimentType as keyof typeof probablityExperimentTypeStoreHeader
+      ];
     const metricProbablity = metricProbablityObj[flagVariation as keyof typeof metricProbablityObj];
     if (stage1metric < metricProbablity.metric1) {
       client?.track("store-accessed", client.getContext());
@@ -171,9 +156,6 @@ export const generateShortenCollectionsPageFunnelExperimentResults = async ({
   setProgress(0);
   setExpGenerator(true);
   let totalPrice = 0;
-  let metric1 = 0;
-  let metric2 = 0;
-  let metric3 = 0;
 
   const experimentType: string = experimentTypeObj.experimentType;
 
@@ -183,30 +165,30 @@ export const generateShortenCollectionsPageFunnelExperimentResults = async ({
       "old-long-collections-page"
     );
 
+    const metricProbablityObj =
+      probablityExperimentTypeShortenCollection[
+        experimentType as keyof typeof probablityExperimentTypeShortenCollection
+      ];
+    const metricProbablity = metricProbablityObj[flagVariation as keyof typeof metricProbablityObj];
+
     if (flagVariation === "old-long-collections-page") {
-      metric1 = 50;
-      metric2 = 40;
-      metric3 = 20;
       totalPrice = Math.floor(Math.random() * (300 - 200 + 1)) + 200;
     }
     if (flagVariation === "new-shorten-collections-page") {
-      metric1 = 70;
-      metric2 = 60;
-      metric3 = 30;
       totalPrice = Math.floor(Math.random() * (500 - 300 + 1)) + 300;
     }
 
     let stage1metric = Math.random() * 100;
 
-    if (stage1metric < metric1) {
+    if (stage1metric < metricProbablity.metric1) {
       client?.track("item-added", client.getContext());
       let stage2metric = Math.random() * 100;
 
-      if (stage2metric < metric2) {
+      if (stage2metric < metricProbablity.metric2) {
         client?.track("cart-accessed", client.getContext());
         let stage3metric = Math.random() * 100;
 
-        if (stage3metric < metric3) {
+        if (stage3metric < metricProbablity.metric3) {
           client?.track("customer-checkout", client.getContext());
           client?.track("in-cart-total-price", client.getContext(), totalPrice);
         }
