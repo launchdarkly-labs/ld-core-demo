39d806ca6606fb85daf89ce6ad639cbeeedacc17
Ahmed Qadri <ahmedhqadri@Ahmeds-MacBook-Pro-2.local>
2025-05-23 14:44:32 -0700

    added o11y sdk

--- components/TelemetryProvider.tsx
+++ components/TelemetryProvider.tsx
@@ -6,28 +6,28 @@ import SessionReplay from "@launchdarkly/session-replay";
 const TelemetryWrapper = ({ children }: { children: React.ReactNode }) => {
   const client = useLDClient();
 
-  // useEffect(() => {
+  useEffect(() => {
 
-  //   if (client) {
-  //     register(client);
-  //     client.addHook(new Observability(process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID, {
-  //       serviceName: "launch-investments",
-  //       tracingOrigins: true,
-  //       networkRecording: {
-  //         enabled: true,
-  //         recordHeadersAndBody: true,
-  //         urlBlocklist: [],
-  //       }
-  //     }));
-  //     client.addHook(new SessionReplay(process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID, {
-  //       serviceName: "launch-investments",
-  //       tracingOrigins: true,
-  //       inlineImages: true,
-  //       inlineVideos: true,
-  //       inlineStylesheet: true
-  //     }));
-  //   }
-  // }, [client]);
+    if (client) {
+      register(client);
+      client.addHook(new Observability(process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID, {
+        serviceName: process.env.NEXT_PUBLIC_PROJECT_KEY+"-observability",
+        tracingOrigins: true,
+        networkRecording: {
+          enabled: true,
+          recordHeadersAndBody: true,
+          urlBlocklist: [],
+        }
+      }));
+      client.addHook(new SessionReplay(process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID, {
+        serviceName: process.env.NEXT_PUBLIC_PROJECT_KEY+"-session-replay",
+        tracingOrigins: true,
+        inlineImages: true,
+        inlineVideos: true,
+        inlineStylesheet: true
+      }));
+    }
+  }, [client]);
 
 
 return <>
