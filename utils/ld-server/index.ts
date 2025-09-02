204124698ce1b2d53b1a37915f7e1b70f1d5229a
Vadim Korolik <vkorolik@gmail.com>
2025-09-02 14:55:46 -0700

    feat: add o11y plugin to node backend

--- utils/ld-server/index.ts
+++ utils/ld-server/index.ts
@@ -1,2 +1,3 @@
 export { default as getServerClient } from "./serverClient";
 export * as LDNodeSDK from "@launchdarkly/node-server-sdk";
+export { Observability } from "@launchdarkly/observability-node";
